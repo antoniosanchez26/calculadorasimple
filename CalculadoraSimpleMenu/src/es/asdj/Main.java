11d19a18ef60a53d007d5ad7bc89b73be3025ddb
al036991 <al036991@ieslaencanta.com>
2022-02-11 13:07:19 +0100

    calculadra terminada.

--- CalculadoraSimpleMenu/src/es/asdj/Main.java
+++ CalculadoraSimpleMenu/src/es/asdj/Main.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int numero1 = 0,numero2 = 0,suma = 0,resta = 0,opcion;
+        int numero1 = 0, numero2 = 0, suma = 0, resta = 0, opcion;
         Scanner entrada = new Scanner(System.in);
 
         do {
+
             System.out.println("MENÚ");
             System.out.println("============================");
             System.out.println("1.- Sumar.");
@@ -17,25 +18,30 @@ public class Main {
             System.out.print("Elige una opción: ");
             opcion = entrada.nextInt();
 
-            if (opcion == 1);{
+            if (opcion == 1) {
                 System.out.print("Numero 1: ");
                 numero1 = entrada.nextInt();
                 System.out.print("Numero 2: ");
                 numero2 = entrada.nextInt();
                 suma = numero1 + numero2;
-                System.out.print(numero1 + " + " + numero2 + " = " + suma);
+                System.out.println(numero1 + " + " + numero2 + " = " + suma);
             } else if (opcion == 2) {
                 System.out.print("Numero 1: ");
                 numero1 = entrada.nextInt();
                 System.out.print("Numero 2: ");
                 numero2 = entrada.nextInt();
-                resta = numero1 - numero2;
-                System.out.print(numero1 + " - " + numero2 + " = " + resta);
+                resta = (numero1 - numero2);
+                System.out.println(numero1 + " - " + numero2 + " = " + resta);
+            } else if (opcion != 0){
+                System.out.println();
+                System.out.println("ERROR , da una opcion válida");
             }
+            entrada.nextLine();
 
-        } while (opcion < 0 || opcion > 2);
-
-
-
+            System.out.print("Pulsa INTRO para continuar...");
+            entrada.nextLine();
+            System.out.println();
+        } while (opcion != 0);
+        System.out.println("Apagando calculadora...");
     }
 }
